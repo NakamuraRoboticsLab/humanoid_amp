00c2766087636f2aaf49d59c470b584dcc62ca8c
hezepy <35261848+hezepy@users.noreply.github.com>
2026-01-16 11:22:08 +0400

    Update copyright year and import statements

--- play.py
+++ play.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# Copyright (c) 2022-2026, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
 # All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
@@ -74,13 +74,13 @@ simulation_app = app_launcher.app
 
 """Rest everything follows."""
 
-import gymnasium as gym
 import os
 import random
 import time
-import torch
 
+import gymnasium as gym
 import skrl
+import torch
 from packaging import version
 
 # check for minimum supported skrl version
@@ -105,9 +105,21 @@ from isaaclab.envs import (
     multi_agent_to_single_agent,
 )
 from isaaclab.utils.dict import print_dict
-from isaaclab.utils.pretrained_checkpoint import get_published_pretrained_checkpoint
 
 from isaaclab_rl.skrl import SkrlVecEnvWrapper
+
+# Optional: helper for downloading published pretrained checkpoints.
+# Some Isaac Lab / isaaclab_rl versions do not ship this utility.
+try:
+    from isaaclab_rl.utils.pretrained_checkpoint import get_published_pretrained_checkpoint
+except ModuleNotFoundError:
+    try:
+        from isaaclab_tasks.utils.pretrained_checkpoint import get_published_pretrained_checkpoint  # type: ignore
+    except ModuleNotFoundError:
+        try:
+            from isaaclab.utils.pretrained_checkpoint import get_published_pretrained_checkpoint  # type: ignore
+        except ModuleNotFoundError:
+            get_published_pretrained_checkpoint = None
 
 import isaaclab_tasks  # noqa: F401
 from isaaclab_tasks.utils import get_checkpoint_path
@@ -154,6 +166,12 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, expe
     print(f"[INFO] Loading experiment from directory: {log_root_path}")
     # get checkpoint path
     if args_cli.use_pretrained_checkpoint:
+        if get_published_pretrained_checkpoint is None:
+            raise ModuleNotFoundError(
+                "Pretrained checkpoint helper is unavailable in this Isaac Lab installation. "
+                "Re-run without --use_pretrained_checkpoint, or upgrade/install the Isaac Lab RL utilities that "
+                "provide get_published_pretrained_checkpoint."
+            )
         resume_path = get_published_pretrained_checkpoint("skrl", train_task_name)
         if not resume_path:
             print("[INFO] Unfortunately a pre-trained checkpoint is currently unavailable for this task.")
